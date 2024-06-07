50a514938a3b84859979619edf34acc582063ea0
Sherezz Grant <rezziemaven@gmail.com>
2024-06-07 16:19:22 -0400

    Add menu hamburger script

---
+++ src/scripts/menu.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+  // add class expanded to '.nav-links' div
+  document.querySelector(".nav-links").classList.toggle("expanded");
+});
